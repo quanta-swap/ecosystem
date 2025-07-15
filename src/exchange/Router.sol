452d87390a47e00999671469603c10e34ede78a3
definenoob <egd@summoner.to>
2025-07-15 19:35:16 -0400

    baby steps

--- src/exchange/Router.sol
+++ src/exchange/Router.sol
@@ -1,2 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+
+interface IRouter {
+
+    struct SwapOrder {
+        address tokenIn;
+        address tokenOut;
+        bool exactIn;
+        uint64 amount;
+        uint64 limit;
+        address recipient;
+        bool useCPMM;
+        bool useCLMM;
+        uint64[] orders;
+    }
+
+
+    /* Supports exactIn / exactOut */
+    function swap(
+        uint64 freeToken,
+        SwapOrder[] calldata swaps
+    ) external returns (uint64 amountCalculated);
+
+}
